346271117aa6000a821acc2b888a01d1b1b3c731
tedaringo <tedaringo628@gmail.com>
2024-07-18 16:09:56 +0300

    Changed the knexfile to fix the export error

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,9 @@
-require('dotenv').config(); 
+require('dotenv').config();
 
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
-export const development = {
+const development = {
   client: 'mysql2',
   connection: {
     host: process.env.DB_HOST,
@@ -12,52 +12,59 @@ export const development = {
     database: process.env.DB_NAME,
   },
   migrations: {
-    directory: './migrations', 
-    tableName: 'knex_migrations'
-  },
-  seeds: {
-  directory: './seeds'
-  },
-  pool: {
-    min: 2,
-    max: 10
-  }
-};
-export const staging = {
-  client: 'mysql2',
-  connection: {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-  },
-  pool: {
-    min: 2,
-    max: 10
-  },
-  migrations: {
-    directory: './migrations', 
-    tableName: 'knex_migrations'
-  }
-};
-export const production = {
-  client: 'mysql2',
-  connection: {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-  },
-  pool: {
-    min: 2,
-    max: 10
-  },
-  migrations: {
-    directory: './migrations', 
+    directory: './migrations',
     tableName: 'knex_migrations'
   },
   seeds: {
     directory: './seeds'
-    },
+  },
+  pool: {
+    min: 2,
+    max: 10
+  }
+};
 
+const staging = {
+  client: 'mysql2',
+  connection: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  },
+  pool: {
+    min: 2,
+    max: 10
+  },
+  migrations: {
+    directory: './migrations',
+    tableName: 'knex_migrations'
+  }
+};
+
+const production = {
+  client: 'mysql2',
+  connection: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  },
+  pool: {
+    min: 2,
+    max: 10
+  },
+  migrations: {
+    directory: './migrations',
+    tableName: 'knex_migrations'
+  },
+  seeds: {
+    directory: './seeds'
+  }
+};
+
+module.exports = {
+  development,
+  staging,
+  production
 };
