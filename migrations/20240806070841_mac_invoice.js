5fd46eb05d13a11c23353aa61d3d8b660bc23f9c
Ted Aringo <tedaringo@Teds-MacBook-Pro.local>
2024-08-06 10:29:15 +0300

    Switched to new laptop, running migrations and new env file

---
+++ migrations/20240806070841_mac_invoice.js
@@ -0,0 +1,30 @@
+// migrations/20240527052015_create_invoices_view.js
+
+exports.up = function(knex) {
+    return knex.schema.raw(`
+      CREATE VIEW invoicesView AS
+      SELECT 
+        invoices.invoiceID,
+        tenants.tenant_name,
+        invoices.tenantID,
+        tenants.phone_number,
+        invoices.amountDue,
+        invoices.dateOfInvoice,
+        invoices.dateDue,
+        invoices.status,
+        invoices.comment,
+        periods.month,
+        periods.year
+      FROM 
+        invoices
+      LEFT JOIN 
+        tenants ON invoices.tenantID = tenants.tenantID
+      LEFT JOIN
+        periods ON invoices.periodID = periods.periodID;
+    `);
+  };
+  
+  exports.down = function(knex) {
+    return knex.schema.raw('DROP VIEW IF EXISTS invoicesView');
+  };
+  
